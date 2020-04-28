8c38bd6d0e425da7b4f4c8b776c34420f57db88d
salomaogit <63564287+salomaogit@users.noreply.github.com>
2020-04-27 21:56:00 -0400

    alteração nas fotos

--- norte/view/quemsomos.php
+++ norte/view/quemsomos.php
@@ -1,7 +1,7 @@
 <div class="container" id="faleconosco">
     <div>
         <div>
-            <img src="img/loja/loja29.jpeg" alt="Foto" width="100%">
+            <img src="img/loja/loja29.png" alt="Foto" width="100%">
             <div class="col-sm-12 text-center fale">
                 <h6>Somos uma empresa voltada para o serviço de manutenção eletrônica, hardware e software, em equipamentos eletònicos e de informática.</h6>
             </div>
@@ -12,12 +12,12 @@
                     <div class="row">
                         <div class="col-sm-12 text-center"><h1>Instalações</h1></div>
                         <div class="col-sm-6 text-justify">
-                            <div><img src="img/loja/loja24.jpeg" alt="Foto" width="100%"></div>
+                            <div><img src="img/loja/loja24.png" alt="Foto" width="100%"></div>
                             <p>Cituada a rua Chico Mendes, 15 Riacho Doce - Cidade Nova I, na cidade de Manaus-AM, nossa loja conta com uma equipe preparada para efetuar qualquer tipo de conserto eletônico, manutenção e reparos diversos, além de instalação de softwares diversos.</p>
                         </div>
                         <div class="col-sm-6 text-justify">
                             <p>Contamos com bancadas e materiais de preparo para fazer sua manutenção sem riscos de danificar ou arranhar seu equipamento, pois, sabemos que seu propósito ao trazê-lo em nossa loja é o de resolver seu problema e não de causar danos e prejuízos aos nossos clientes, pois, o cliente é o nosso ator principal em nosso processo.</p>
-                            <div><img src="img/loja/loja27.jpeg" alt="Foto" width="100%"></div>
+                            <div><img src="img/loja/loja27.png" alt="Foto" width="100%"></div>
                         </div>
                     </div>
                 </section>
@@ -25,13 +25,13 @@
                     <div class="row">
                         <div class="col-sm-12 text-center"><h1>Missão, Visão e Valores</h1></div>
                         <div class="col-sm-4">
-                            <img src="img/loja/loja23.jpeg" alt="Foto" width="100%">
+                            <img src="img/loja/loja23.png" alt="Foto" width="100%">
                         </div>
                         <div class="col-sm-4">
-                            <img src="img/loja/loja28.jpeg" alt="Foto" width="100%">
+                            <img src="img/loja/loja28.png" alt="Foto" width="100%">
                         </div>
                         <div class="col-sm-4">
-                            <img src="img/loja/loja25.jpeg" alt="Foto" width="100%">
+                            <img src="img/loja/loja25.png" alt="Foto" width="100%">
                         </div>
                         <div class="col-sm-12 text-justify">
                             <p>Temos como Missão: Atender as necessidades dos clientes com diagnósticos precisos e corretos afim de fornecer o melhor servico de assistência técnica e eletrônica!</p>
@@ -49,7 +49,7 @@
             <div class="text-justify"><p>Trabalhamos com todas as marcas e modelos de eletro-eletônicos. Fornecemos manutenção em hardware, software e eletrônica além de peças e assessórios em geral para TVs, monitores, projetores, desktops, notebooks, nobreaks, DVDs, blueRays, impressoras, aparelhos de som, tablets, celulares e muito mais.</p></div>    
         </div>
         <div class="col-sm-7">
-            <img class="img-fluid" src="img/panfleto.jpeg" alt="Panfleto">
+            <img class="img-fluid" src="img/panfleto.png" alt="Panfleto">
         </div>
         <div class="col-sm-5">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.2606826833!2d-59.9816874857155!3d-3.0245210408439878!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x926c1978776295bf%3A0xd8a910e958bc74ee!2sNorte%20Assist%C3%AAncia%20T%C3%A9cnica%20e%20Eletr%C3%B4nica!5e0!3m2!1spt-BR!2sbr!4v1574985410773!5m2!1spt-BR!2sbr" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
