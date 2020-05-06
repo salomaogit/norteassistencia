f0f674ccf76fd0a24919ffee85f460f0c8c82a1d
salomaogit <63564287+salomaogit@users.noreply.github.com>
2020-05-06 14:35:58 -0400

    commit menu

--- norte/view/menu.php
+++ norte/view/menu.php
@@ -5,7 +5,7 @@
         <!-- <li class="nav-item"><a href="?pag=faleconosco" class="nav-link">Fale Conosco</a></li> -->
         <li class="nav-item"><a href="?pag=servicos" class="nav-link">Serviços</a></li>
     </ul>
-    <form class="btn-group" action="#" method="post">
+    <form id="busca" class="btn-group" action="#" method="post">
         <input type="text">
         <button class="btn btn-primary"><i class="fas fa-search"></i></button>
     </form>
